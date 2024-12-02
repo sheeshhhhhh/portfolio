b2e37449a36684d06c1523749266ce52a195a012
renn <renatodsantosjr9@gmail.com>
2024-12-02 12:28:51 +0800

    it;s not working but i think the allowed host is the problem

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-qs89$pmey$dn@**8m7r92a#@rz882@t9-63p++x1p79xk(+j8y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://portfolio-2655.onrender.com/'
+]
 
 
 # Application definition
