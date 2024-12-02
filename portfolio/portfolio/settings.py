2fe718693fcfd873dd84a2ad14de9682d8e7f21f
renn <renatodsantosjr9@gmail.com>
2024-12-02 12:11:20 +0800

    fix the url config error in deployment

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qs89$pmey$dn@**8m7r92a#@rz882@t9-63p++x1p79xk(+j8y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'portfolio'
 ]
+
+ROOT_URLCONF = 'portfolio.urls'
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
