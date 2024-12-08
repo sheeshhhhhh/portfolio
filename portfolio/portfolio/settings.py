2819bb1821a9c451370dc23791a08be3a4491253
renn <renatodsantosjr9@gmail.com>
2024-12-08 10:49:04 +0800

    making debug False

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qs89$pmey$dn@**8m7r92a#@rz882@t9-63p++x1p79xk(+j8y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'portfolio-2655.onrender.com',
