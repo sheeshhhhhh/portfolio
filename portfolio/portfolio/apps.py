a852f853cd4b6983d675a1bd52d36483bf966c3b
renn <renatodsantosjr9@gmail.com>
2024-12-09 10:55:46 +0800

    remove app.py

--- portfolio/portfolio/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class PortfolioConfig(AppConfig):
-    name = 'portfolio'
-    path = '/home/RenatoSantos/portfolio/portfolio/portfolio'  # Adjust the path if necessary
