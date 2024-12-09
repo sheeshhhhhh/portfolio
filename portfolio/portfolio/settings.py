69583030c82cdaf01a57c41f8f4aac0e056d40cf
renn <renatodsantosjr9@gmail.com>
2024-12-09 11:29:10 +0800

    remove duplicate

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -49,7 +49,6 @@ CSRF_TRUSTED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'portfolio',
     'portfolio.apps.PortfolioConfig',
     'django.contrib.admin',
     'django.contrib.auth',
