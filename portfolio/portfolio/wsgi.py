c2efc2688256b9d87f20a455d937804658802013
renn <renatodsantosjr9@gmail.com>
2024-12-09 10:37:22 +0800

    changes the wsgi

--- portfolio/portfolio/wsgi.py
+++ portfolio/portfolio/wsgi.py
@@ -8,16 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
-# Adjust the path to your project directory
-project_home = '/home/RenatoSantos/portfolio'
-if project_home not in sys.path:
-    sys.path = [project_home] + sys.path
-
-# Set the DJANGO_SETTINGS_MODULE
-os.environ['DJANGO_SETTINGS_MODULE'] = 'portfolio.portfolio.settings'
-
-# Load the WSGI application
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
+
 application = get_wsgi_application()
