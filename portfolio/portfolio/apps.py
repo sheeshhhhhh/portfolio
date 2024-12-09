3c6fe02b4340b6dc0e7759e3361a7a3c8ed32b99
renn <renatodsantosjr9@gmail.com>
2024-12-09 11:26:12 +0800

    dsad

---
+++ portfolio/portfolio/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class PortfolioConfig(AppConfig):
+    name = 'portfolio'
+    path = '/home/RenatoSantos/portfolio/portfolio/portfolio'  # Adjust the path if necessary
