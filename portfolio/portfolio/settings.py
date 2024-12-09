58d88468cebe686866f86fa862eff4905631d36c
renn <renatodsantosjr9@gmail.com>
2024-12-09 10:51:13 +0800

    added app.,py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -50,6 +50,7 @@ CSRF_TRUSTED_ORIGINS = [
 
 INSTALLED_APPS = [
     'portfolio',
+    'portfolio.apps.PortfolioConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
